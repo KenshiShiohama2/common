efd59d26449bb9ee3483028263c9797569e7e0b7
KenshiShiohama2 <k.shiohama00@gmail.com>
2021-03-19 14:24:07 +0900

    test

--- BookRepositoryImpl.java
+++ BookRepositoryImpl.java
@@ -14,7 +14,7 @@ import com.example.demo.model.Book;
 public interface BookRepositoryImpl extends JpaRepository <Book, Integer> {
 	List<Book> findAll();
 	Optional<Book> findById(Integer id);
-	List<Book> findByPublisher(String publisher);
+	List<Book> findByPublisher2(String publisher);
 	List<Book> findByPriceGreaterThanEqual(int price);
 //	List<BookTitleOnly> findByPublisher(String publisher);
 
